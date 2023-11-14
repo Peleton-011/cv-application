51eb96c6d631d4db09e2a5b6750795212b260bf9
Nico <nicodiazan@gmail.com>
2023-11-14 15:44:22 +0100

    Refactor GeneralInfoInput to make it reusable

--- src/components/GeneralInfoInput.jsx
+++ src/components/GeneralInfoInput.jsx
@@ -6,45 +6,42 @@ const GeneralInfoInput = () => {
 		<div>
 			<Form
 				formName="General Information"
-				inputs={[
-					{
-						inputName: "name",
-						inputID: "name",
-						label: "Name",
-                        type: "text"
-					},
-					{
-						inputName: "address",
-						inputID: "address",
-						label: "Address",
-                        type: "text"
-
-					},
-					{
-						inputName: "phoneNumber",
-						inputID: "phoneNumber",
-						label: "Phone Number",
-                        type: "phone"
-
-					},
-					{
-						inputName: "email",
-						inputID: "email",
-						label: "Email",
-                        type: "email"
-
-					},
-				]}
+				inputs={inputList}
 				submit="Submit Now!"
 				submitHandler={(e) => {
 					e.preventDefault();
 					console.log("Submitted!");
-				}
-                    
-            }
+				}}
 			/>
 		</div>
 	);
 };
+
+const inputList = [
+	{
+		inputName: "name",
+		inputID: "name",
+		label: "Name",
+		type: "text",
+	},
+	{
+		inputName: "address",
+		inputID: "address",
+		label: "Address",
+		type: "text",
+	},
+	{
+		inputName: "phoneNumber",
+		inputID: "phoneNumber",
+		label: "Phone Number",
+		type: "phone",
+	},
+	{
+		inputName: "email",
+		inputID: "email",
+		label: "Email",
+		type: "email",
+	},
+];
 
 export default GeneralInfoInput;
