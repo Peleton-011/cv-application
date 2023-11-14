8215a8b50ce4fd0e22c370994172076e1d9217b8
Nico <nicodiazan@gmail.com>
2023-11-14 15:29:43 +0100

    Refactor the test environment to better compartmentalize code

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,11 @@
 import "./App.css";
-import Form from "./components/Form";
+import GeneralInfoInput from "./components/GeneralInfoInput";
 
 function App() {
 	return (
 		<>
 			<h1>Henlo</h1>
-			<Form
-				formName="General Information"
-				inputs={[
-					{
-						inputName: "name",
-						inputID: "name",
-						label: "Name",
-					},
-					{
-						inputName: "address",
-						inputID: "address",
-						label: "Address",
-					},
-					{
-						inputName: "phoneNumber",
-						inputID: "phoneNumber",
-						label: "Phone Number",
-					},
-					{
-						inputName: "email",
-						inputID: "email",
-						label: "Email",
-					},
-				]}
-				submit="Submit Now!"
-				submitHandler={(e) => {
-					e.preventDefault();
-					console.log("Submitted!");
-				}}
-			/>
+			<GeneralInfoInput />
 		</>
 	);
 }
--- src/components/GeneralInfoInput.jsx
+++ src/components/GeneralInfoInput.jsx
@@ -1,12 +1,40 @@
 import React from "react";
-import Input from "./Input";
+import Form from "./Form";
 
 const GeneralInfoInput = () => {
 	return (
 		<div>
-			<h3>General Information</h3>
-            
-        </div>
+			<Form
+				formName="General Information"
+				inputs={[
+					{
+						inputName: "name",
+						inputID: "name",
+						label: "Name",
+					},
+					{
+						inputName: "address",
+						inputID: "address",
+						label: "Address",
+					},
+					{
+						inputName: "phoneNumber",
+						inputID: "phoneNumber",
+						label: "Phone Number",
+					},
+					{
+						inputName: "email",
+						inputID: "email",
+						label: "Email",
+					},
+				]}
+				submit="Submit Now!"
+				submitHandler={(e) => {
+					e.preventDefault();
+					console.log("Submitted!");
+				}}
+			/>
+		</div>
 	);
 };
 
