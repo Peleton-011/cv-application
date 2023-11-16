18f65b59645372615126d0b2efaa7df387fc5037
Nico Diaz <nicodiazan@gmail.com>
2023-11-16 12:06:36 +0100

    Add experience info to the CV display + other minor improvements to it

--- src/components/CVOutput.jsx
+++ src/components/CVOutput.jsx
@@ -12,19 +12,48 @@ const CVOutput = ({ generalInfo, educationInfo, experienceInfo }) => {
 					<span>{email}</span>
 				</div>
 			</div>
-			<div>
-				{educationInfo.map((info) => {
-					const { schoolName, degreeName, startDate, id } = info;
-					return (
-						<div key={id}>
-							<h3>
-								{degreeName} - {schoolName}
-							</h3>
-							<span>{startDate}</span>
-						</div>
-					);
-				})}
-			</div>
+			{educationInfo.length ? (
+				<div>
+					<h2>Education</h2>
+					{educationInfo.map((info) => {
+						const { schoolName, degreeName, startDate, id } = info;
+						return (
+							<div key={id}>
+								<h3>
+									{degreeName} - {schoolName}
+								</h3>
+								<span>{startDate}</span>
+							</div>
+						);
+					})}
+				</div>
+			) : null}
+			{experienceInfo.length ? (
+				<div>
+					<h2>Experience</h2>
+					{experienceInfo.map((info) => {
+						const {
+							companyName,
+							positionName,
+							startDate,
+							endDate,
+							description,
+							id,
+						} = info;
+						return (
+							<div key={id}>
+								<h3>
+									{positionName} at {companyName}
+								</h3>
+								<span>
+									{startDate} - {endDate || "current"}
+								</span>
+								<p>{description}</p>
+							</div>
+						);
+					})}
+				</div>
+			) : null}
 		</section>
 	);
 };
